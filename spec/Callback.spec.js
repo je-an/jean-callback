a88fe386c7ba2324196dbc904ad441e3e9a3fcd4
je-an <mail@jeremiasandris.de>
2017-10-02 15:59:44 +0200

    Spec description changed

--- spec/Callback.spec.js
+++ spec/Callback.spec.js
@@ -11,21 +11,26 @@ define([
         });
         describe("Methods", function () {
             describe("Callback Constructor", function () {
-                it("TODO: Check if all members are available | EXPECTATION: Callback has all necessary members", function () {
+                it("All necessary members are available", function () {
                     var numberOfMembers = 2;
                     expect(Object.keys(cb).length).toEqual(numberOfMembers);
+                    expect(cb.id).not.toBeUndefined();
+                    expect(cb._callbacks).not.toBeUndefined();
                 });
-                it("TODO: Check if all methods are available | EXPECTATION: Callback has all necessary  methods", function () {
+                it("All necessary methods are available", function () {
                     var numberOfMethods = 3;
                     var methodCount = Object.keys(Object.getPrototypeOf(cb)).length;
                     expect(methodCount).toEqual(numberOfMethods);
+                    expect(cb.registerFunction).not.toBeUndefined();
+                    expect(cb.unregisterFunction).not.toBeUndefined();
+                    expect(cb.broadcastToFunctions).not.toBeUndefined();
                 });
-                it("TODO: Pass valid parameters to constructor | EXPECTATION: Callback is initialized successful", function () {
+                it("initialises successfully, if valid options are passed", function () {
                     var c = new Callback({ id: "callback" });
                     expect(c.id).toEqual("callback");
                     expect(c._callbacks).toEqual([]);
                 });
-                it("TODO: Pass invalid parameter as argument to constructor | EXPECTATION: Argument is ignored, Callback is standard initialised", function () {
+                it("Does standard initialisation, if something invalid is passed to constructor", function () {
                     var c = new Callback(function () { });
                     expect(c.id).toEqual("");
                     c = new Callback(true);
@@ -39,7 +44,7 @@ define([
                     c = new Callback(undefined);
                     expect(c.id).toEqual("");
                 });
-                it("TODO: Pass invalid parameter for id to constructor | EXPECTATION: Id is ignored and default initialised", function () {
+                it("Does standard initialisation, if something invalid is passed to constructor as id", function () {
                     var c = new Callback({ id: 1 });
                     expect(c.id).toEqual("");
                     var c = new Callback({ id: false });
@@ -55,11 +60,11 @@ define([
                 });
             });
             describe("Callback.prototype.registerFunction", function () {
-                it("TODO: Register a callback | EXPECTATION: Callback registered successfully", function () {
+                it("Registers a callback", function () {
                     expect(cb.registerFunction(function () { })).toBe(true);
                     expect(cb._callbacks.length).toEqual(1);
                 });
-                it("TODO: Register multiple callbacks | EXPECTATION: Callbacks registered successfully", function () {
+                it("Registers multiple callbacks", function () {
                     expect(cb.registerFunction(function () { })).toBe(true);
                     expect(cb._callbacks.length).toEqual(1);
                     expect(cb.registerFunction(function () { })).toBe(true);
@@ -69,7 +74,7 @@ define([
                     expect(cb.registerFunction(function () { })).toBe(true);
                     expect(cb._callbacks.length).toEqual(4);
                 });
-                it("TODO: Register something else as an callback | EXPECTATION: Nothing will be registered", function () {
+                it("Registers nothing, if something invalid is passed for registration", function () {
                     expect(cb.registerFunction(10)).toBe(false);
                     expect(cb._callbacks.length).toEqual(0);
                     expect(cb.registerFunction("name")).toBe(false);
@@ -85,14 +90,14 @@ define([
                 });
             });
             describe("Callback.prototype.unregisterFunction", function () {
-                it("TODO: Unregister a registered callback | EXPECTATION: Callback will be removed successfully", function () {
+                it("Unregister a registered callback", function () {
                     cb.registerFunction(fnOne);
                     cb.registerFunction(fnTwo);
                     cb.registerFunction(fnThree);
                     expect(cb.unregisterFunction(fnOne)).toBe(true);
                     expect(cb._callbacks.length).toEqual(2);
                 });
-                it("TODO: Try to unregister a non registered function | EXPECTATION: Nothing will be removed", function () {
+                it("Does nothing, if a function shall be unregistered, which is not registered before", function () {
                     cb.registerFunction(fnOne);
                     cb.registerFunction(fnTwo);
                     cb.registerFunction(fnThree);
@@ -101,7 +106,7 @@ define([
                 });
             });
             describe("Callback.prototype.broadcastToFunctions", function () {
-                it("TODO: Broadcast to registered callbacks | EXPECTATION: Callbacks will be notified", function () {
+                it("Broadcasts values to registered functions", function () {
                     var fnOne = function (param) { // jscs:ignore
                         expect(param).toBe(true);
                     },
@@ -116,7 +121,7 @@ define([
                     cb.registerFunction(fnThree);
                     expect(cb.broadcastToFunctions(true)).toBe(true);
                 });
-                it("TODO: Broadcast null to registered callbacks | EXPECTATION: Callbacks will be notified", function () {
+                it("Broadcasts null values to functions", function () {
                     var fnOne = function (param) { // jscs:ignore
                         expect(param).toBe(null);
                     },
@@ -131,7 +136,7 @@ define([
                     cb.registerFunction(fnThree);
                     expect(cb.broadcastToFunctions(null)).toBe(true);
                 });
-                it("TODO: Broadcast undefined registered callbacks | EXPECTATION: Callbacks will be notified", function () {
+                it("Broadcasts undefined values to functions", function () {
                     var fnOne = function (param) { // jscs:ignore
                         expect(param).toBe(undefined);
                     },
@@ -146,81 +151,79 @@ define([
                     cb.registerFunction(fnThree);
                     expect(cb.broadcastToFunctions(undefined)).toBe(true);
                 });
-                it("TODO: Broadcast although nothing is registered | EXPECTATION: Nothing is notified", function () {
+                it("Broadcasts nothing, if no functions are registered", function () {
                     expect(cb.broadcastToFunctions(true)).toBe(false);
                 });
             });
         });
         describe("Behaviour", function () {
-            describe("Callback", function () {
-                it("should register three functions, broadcast to them, and remove the second of the registered function", function () {
-                    var cb = new Callback();
-                    var fnOne = function (id) {
-                        expect(id).toEqual("id");
-                    };
-                    var fnTwo = function (id) {
-                        expect(id).toEqual("id");
-                    };
-                    var fnThree = function (id) {
-                        expect(id).toEqual("id");
-                    };
-                    cb.registerFunction(fnOne);
-                    cb.registerFunction(fnTwo);
-                    cb.registerFunction(fnThree);
-                    expect(cb._callbacks.length).toEqual(3);
-                    var d = cb.broadcastToFunctions("id");
-                    expect(d).toBe(true);
-                    cb.unregisterFunction(fnTwo);
-                    expect(cb._callbacks.length).toEqual(2);
-                });
-                it("should register three functions, remove one, and broadcast to remaining", function () {
-                    var count = 0;
-                    var cb = new Callback();
-                    var fnOne = function (param) { // jscs:ignore
+            it("Registers three functions, broadcasts and removes the second of the registered functions", function () {
+                var cb = new Callback();
+                var fnOne = function (id) {
+                    expect(id).toEqual("id");
+                };
+                var fnTwo = function (id) {
+                    expect(id).toEqual("id");
+                };
+                var fnThree = function (id) {
+                    expect(id).toEqual("id");
+                };
+                cb.registerFunction(fnOne);
+                cb.registerFunction(fnTwo);
+                cb.registerFunction(fnThree);
+                expect(cb._callbacks.length).toEqual(3);
+                var d = cb.broadcastToFunctions("id");
+                expect(d).toBe(true);
+                cb.unregisterFunction(fnTwo);
+                expect(cb._callbacks.length).toEqual(2);
+            });
+            it("Registers three functions, removes one and broadcasts to remaining function", function () {
+                var count = 0;
+                var cb = new Callback();
+                var fnOne = function (param) { // jscs:ignore
+                    expect(param).toBe(true);
+                    count++;
+                },
+                    fnTwo = function (param) { // jscs:ignore
                         expect(param).toBe(true);
                         count++;
                     },
-                        fnTwo = function (param) { // jscs:ignore
-                            expect(param).toBe(true);
-                            count++;
-                        },
-                        fnThree = function (param) { // jscs:ignore
-                            expect(param).toBe(true);
-                            count++;
-                        };
-                    cb.registerFunction(fnOne);
-                    cb.registerFunction(fnTwo);
-                    cb.registerFunction(fnThree);
-                    cb.unregisterFunction(fnOne);
-                    expect(cb.broadcastToFunctions(true)).toBe(true);
-                    expect(count).toEqual(2);
-                });
-                it("should register three functions, remove one, broadcast to remaining, and add the remove one again",function(){
-                    var count = 0;
-                    var cb = new Callback();
-                    var fnOne = function (param) { // jscs:ignore
+                    fnThree = function (param) { // jscs:ignore
+                        expect(param).toBe(true);
+                        count++;
+                    };
+                cb.registerFunction(fnOne);
+                cb.registerFunction(fnTwo);
+                cb.registerFunction(fnThree);
+                cb.unregisterFunction(fnOne);
+                expect(cb.broadcastToFunctions(true)).toBe(true);
+                expect(count).toEqual(2);
+            });
+            it("Registers three functions, removes one, broadcasts to remaining, and add the removed one again", function () {
+                var count = 0;
+                var cb = new Callback();
+                var fnOne = function (param) { // jscs:ignore
+                    expect(param).toBe(true);
+                    count++;
+                },
+                    fnTwo = function (param) { // jscs:ignore
                         expect(param).toBe(true);
                         count++;
                     },
-                        fnTwo = function (param) { // jscs:ignore
-                            expect(param).toBe(true);
-                            count++;
-                        },
-                        fnThree = function (param) { // jscs:ignore
-                            expect(param).toBe(true);
-                            count++;
-                        };
-                    cb.registerFunction(fnOne);
-                    cb.registerFunction(fnTwo);
-                    cb.registerFunction(fnThree);
-                    cb.unregisterFunction(fnOne);
-                    expect(cb.broadcastToFunctions(true)).toBe(true);
-                    expect(count).toEqual(2);
-                    count = 0;
-                    cb.registerFunction(fnOne);
-                    expect(cb.broadcastToFunctions(true)).toBe(true);
-                    expect(count).toEqual(3);
-                });
+                    fnThree = function (param) { // jscs:ignore
+                        expect(param).toBe(true);
+                        count++;
+                    };
+                cb.registerFunction(fnOne);
+                cb.registerFunction(fnTwo);
+                cb.registerFunction(fnThree);
+                cb.unregisterFunction(fnOne);
+                expect(cb.broadcastToFunctions(true)).toBe(true);
+                expect(count).toEqual(2);
+                count = 0;
+                cb.registerFunction(fnOne);
+                expect(cb.broadcastToFunctions(true)).toBe(true);
+                expect(count).toEqual(3);
             });
         });
     });
